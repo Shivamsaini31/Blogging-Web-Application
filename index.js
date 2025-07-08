3c5f78784ee750406089b583b0d37d6b799f8aef
Shivamsaini31 <sainishivam31122005@gmail.com>
2025-07-08 22:07:31 +0530

    feat: dynamically generate blog cards with thumbnail and content per category

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
-
+// app.use('/submissions', express.static(path.join(__dirname, 'submissions')));
+app.use("/submissions", express.static("submissions"));
 app.set("view engine", "ejs");
 
 const storage = multer.diskStorage({
@@ -45,30 +46,37 @@ app.get("/:category", (req, res) => {
     if (!sz)
       res.render("categoryWiseBlogs.ejs", {
         category: category,
-        blogs:[],
+        blogs: [],
       });
     authors.forEach((author) => {
       const authorPath = path.join(categoryPath, author);
       fs.readdir(authorPath, (err, titles) => {
         if (titles) {
+          let titleCount = titles.length;
           titles.forEach((title) => {
-            blogs.push({
-              author,
-              title,
-              path: path.join(authorPath, title),
-            });
+            fs.readFile(
+              path.join(authorPath, title, "blogContent.txt"),
+              (err, blogContent) => {
+                blogs.push({
+                  author: author,
+                  title: title,
+                  path: "/" + path.join(authorPath, title),
+                  blogContent: blogContent,
+                });
+                titleCount--;
+                if (titleCount === 0) sz--;
+                if (sz === 0) {
+                  console.log(blogs);
+                  res.render("categoryWiseBlogs", {
+                    category: category,
+                    blogs: blogs,
+                  });
+                }
+              }
+            );
           });
         }
-        sz--;
-      if (sz == 0) {
-        console.log(blogs);
-        res.render("categoryWiseBlogs", {
-          category: category,
-          blogs:blogs,
-        });
-      }
       });
-      
     });
   });
 });
@@ -83,12 +91,14 @@ app.post("/submitBlog", upload.single("thumbnail"), (req, res) => {
   const newPath = _dirname + "/" + req.file.filename;
   var content = req.body["blogContent"];
   fs.mkdir(_dirname, { recursive: true }, (err) => {
-    if (err) return res.status(500).send("Error creating directory: " + err);
+    if (err)
+      return res.status(500).send("Error creating directory: " + err.message);
 
     fs.rename(oldPath, newPath, (err) => {
-      if (err) return res.status(500).send("Error moving file: ", err);
+      if (err) return res.status(500).send("Error moving file: " + err.message);
       fs.writeFile(_dirname + "/blogContent.txt", content, (err) => {
-        if (err) return res.status(500).send("Error writing file: " + err);
+        if (err)
+          return res.status(500).send("Error writing file: " + err.message);
         res.send("Blog and Image saved successfully!");
       });
     });
