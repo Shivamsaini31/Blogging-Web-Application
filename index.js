944afd642b77a60467541cf8e867c8e14147e094
Shivamsaini31 <sainishivam31122005@gmail.com>
2025-07-18 18:22:19 +0530

    Fix image rendering issues and add new blogs to all categories

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import express from "express";
 import fs from "fs";
 import multer from "multer";
 import path from "path";
-import {dirname} from "path";
-import  {fileURLToPath}  from "url";
-const _fileName=fileURLToPath(import.meta.url);
-const _dirName=dirname(_fileName);
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const _fileName = fileURLToPath(import.meta.url);
+const _dirName = dirname(_fileName);
 const app = express();
 const port = 3000;
 
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(_dirName, "public")));
 // app.use('/submissions', express.static(path.join(__dirname, 'submissions')));
-app.use("/submissions", express.static(path.join(_dirName,"submissions")));
+app.use("/submissions", express.static(path.join(_dirName, "submissions")));
 app.set("view engine", "ejs");
 
 const storage = multer.diskStorage({
@@ -41,7 +41,7 @@ app.get("/createBlog", (req, res) => {
 
 app.get("/:category", (req, res) => {
   const category = req.params.category;
-  const categoryPath = path.join(_dirName,"submissions", category);
+  const categoryPath = path.join(_dirName, "submissions", category);
   fs.readdir(categoryPath, (err, authors) => {
     if (err) return res.status(404).send("Category not found!");
     let blogs = [];
@@ -57,19 +57,25 @@ app.get("/:category", (req, res) => {
         if (titles) {
           let titleCount = titles.length;
           titles.forEach((title) => {
+            const folderPath = path.join(authorPath, title);
+            const files = fs.readdirSync(folderPath);
+            const imgFile = files.find((file) =>
+              /\.(jpe?g|png|webp)$/i.test(file)
+            );
             fs.readFile(
               path.join(authorPath, title, "blogContent.txt"),
               (err, blogContent) => {
                 blogs.push({
                   author: author,
                   title: title,
-                  path: "/" + path.join(authorPath, title),
+                  path: `/submissions/${category}/${author}/${title}`,
+                  image: `/submissions/${category}/${author}/${title}/${imgFile}`,
                   blogContent: blogContent,
                 });
                 titleCount--;
                 if (titleCount === 0) sz--;
                 if (sz === 0) {
-                  console.log(blogs);
+                  // console.log(blogs);
                   res.render("categoryWiseBlogs", {
                     category: category,
                     blogs: blogs,
@@ -89,9 +95,15 @@ app.post("/submitBlog", upload.single("thumbnail"), (req, res) => {
   const safeTitle = title.replace(/[<>:"\/\\|?*]+/g, "_");
   const category = req.body["categories"].trim();
   const author = req.body["author"].trim();
-  const blogDir = path.join(_dirName,"submissions", category, author, safeTitle);
+  const blogDir = path.join(
+    _dirName,
+    "submissions",
+    category,
+    author,
+    safeTitle
+  );
   const oldPath = req.file.path;
-  const newPath = path.join(blogDir,req.file.filename);
+  const newPath = path.join(blogDir, req.file.filename);
   var content = req.body["blogContent"];
   fs.mkdir(blogDir, { recursive: true }, (err) => {
     if (err)
@@ -99,7 +111,7 @@ app.post("/submitBlog", upload.single("thumbnail"), (req, res) => {
 
     fs.rename(oldPath, newPath, (err) => {
       if (err) return res.status(500).send("Error moving file: " + err.message);
-      fs.writeFile(path.join(blogDir,"blogContent.txt"), content, (err) => {
+      fs.writeFile(path.join(blogDir, "blogContent.txt"), content, (err) => {
         if (err)
           return res.status(500).send("Error writing file: " + err.message);
         res.send("Blog and Image saved successfully!");
