1945049be67a37c8b198701aea9374704b4ec779
Shivamsaini31 <sainishivam31122005@gmail.com>
2025-08-13 23:51:18 +0530

    Migrate from fs-based storage to Postgres database
    Implement CRUD functionality and set up separate routes and controllers for cleaner code.

---
+++ controllers/blogControllers.js
@@ -0,0 +1,121 @@
+import { sql } from "../config/db.js";
+
+export const getHomePage = (req, res) => {
+  try {
+    res.render("index.ejs", {
+      category: "Home",
+    });
+  } catch (error) {
+    console.log("Error loading Home Page: ", error);
+    res.sendStatus(500);
+  }
+};
+
+export const getCategoryBlog = async (req, res) => {
+  const { category } = req.params;
+  console.log (category);
+  try {
+    const blogs = await sql`
+    SELECT * FROM blogs
+    WHERE category=${category}
+    ORDER BY created_at DESC
+    `;
+    console.log("blogs:", blogs);
+    res.render("CategoryWiseBlogs",{
+        category:category,
+        blogs:blogs,
+    });
+  } catch (error) {
+    console.log (`Error in fetching blogs for category ${category}: `,error);
+    res.sendStatus(404);
+  }
+};
+// need to update view blog or getBlog function after creating viewBlog.ejs
+export const getBlog = async (req, res) => {
+    const {id}=req.params;
+  try {
+    const blog = await sql`
+        SELECT * FROM blogs
+        WHERE id=${id}
+        `;
+    console.log("Fetched blog: ", blog);
+    
+    res.status(200).render("");
+  } catch (error) {
+    console.log("Error fetching blog: ", error);
+    res.status(500).send("Internal server error!");
+  }
+};
+
+export const createBlog = async (req, res) => {
+    const {title, author,image, blogcontent,category}=req.body;
+    if (!title || !author || !image || !blogcontent || !category) {
+        return res.status(400).send("All fields are required");
+    }
+    try {
+        const submittedBlog=await sql `
+        INSERT INTO blogs(title, author, image, blogcontent, category )
+        VALUES (${title},${author},${image},${blogcontent},${category})
+        RETURNING *
+        `
+        console.log(submittedBlog);
+        res.render("checkBlog",{
+            submittedBlog:submittedBlog[0],
+            category:"none",
+        })
+    } catch (error) {
+        console.log("Error in submitting blog: ",error);
+        return res.status(500).send("Internal Server error!");
+    }
+};
+
+export const deleteBlog = async (req, res) => {
+    const {id}=req.params;
+    
+    try {
+        const deleteBlog=await sql `
+    DELETE FROM blogs WHERE id=${id} 
+    RETURNING *`
+    if(deleteBlog.length===0){
+        return res.status(404).send("Blog does not exists!")
+    }
+    const blogs = await sql`
+    SELECT * FROM blogs
+    WHERE category = ${deleteBlog[0].category}
+    ORDER BY created_at DESC
+`;
+    res.render("CategoryWiseBlogs",{
+        category:deleteBlog[0].category,
+        blogs:blogs,
+    })
+    } catch (error) {
+        res.status(500).json({success:false, message:"Internal Server Error!"})
+    }
+
+};
+
+export const updateBlog = async (req, res) => {
+    const {id}=req.params;
+    const {title, author,image, blogcontent,category}=req.body;
+    if (!title || !author || !image || !blogcontent || !category) {
+        return res.status(400).send("All fields are required");
+    }
+    try {
+        const updatedBlog=await sql `
+        UPDATE blogs
+        SET title=${title},author=${author},image= ${image},blogcontent=${blogcontent},category=${category}
+        WHERE id=${id}
+        RETURNING *
+        `;
+        if(updatedBlog.length===0){
+            console.log("No such blog found!")
+            return res.status(404).send("No such blog found!");
+        }
+        res.redirect(`/api/blogs/${updatedBlog[0].category}`);
+    } catch (error) {
+        console.log("Error in updating blog: ",error);
+        res.status(500).send("Unable to update blog at this moment!");
+    }
+};
+
+
